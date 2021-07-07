669f82d15c0e42664809c7bba3148f0d46a31440
Cristiano Cunha <contato@cristianoprogramador.com>
2021-07-07 20:10:18 -0300

    Base dos exemplos em C#

---
+++ exemplos-chsarp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exemplos_chsarp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
