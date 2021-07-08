854ab4cdfb64cc7f8b75e30cc5bd8cabce312ad1
Cristiano Cunha <contato@cristianoprogramador.com>
2021-07-07 21:45:20 -0300

    Exemplos de DatetimeOffset

--- exemplos-chsarp/DateTimeOffsetExample.cs
+++ exemplos-chsarp/DateTimeOffsetExample.cs
@@ -1,10 +1,31 @@
+using System;
+
 namespace exemplos_chsarp
 {
     public class DateTimeOffsetExample
     {
-         public static void Run()
+        public static void Run()
         {
+            // Data inicial dia 1/1/0001
+            var dataHoraDefaultVazia = new DateTimeOffset();
 
+            // com base com o timezone do computador
+            var dataHoraAtual = DateTimeOffset.Now;
+
+            // em UTC
+            var dataHoraAtualUTC = DateTimeOffset.UtcNow;
+
+            /* 
+                Forma errada de trabalhar com timezone 
+                O problema aqui acontece pois eu não estou
+                obtendo a data usando o UTC(timezone zero)
+            */
+            var dataAtual = DateTimeOffset.Now;
+            var dataAtualOffset6 = new DateTimeOffset(dataAtual.DateTime, new TimeSpan(+6, 0, 0));
+
+            var dataAtualUTC = DateTimeOffset.UtcNow.DateTime;
+            var dataAtualOffset8 = new DateTimeOffset(dataAtualUTC, new TimeSpan(+8, 0, 0));
+            var offset = dataAtualOffset8.Offset;
         }
     }
 }
---
+++ exemplos-chsarp/Readme.md
@@ -0,0 +1,5 @@
+# Tipos do C#
+
+DateTime - Infrmações de data e hora, pegando a data atual do seu computador, com base em seu timezone, ou em UTC(offset zero).
+
+DateTimeOffset - Informações de data, hora e timezone. 
